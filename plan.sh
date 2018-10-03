3d7d3f6f5a2cb4fd5835f86144472f2d443c74cd
Benr Rockwood <benr@cuddletech.com>
2018-10-03 11:58:10 -0700

    Added plan.sh

---
+++ plan.sh
@@ -0,0 +1,9 @@
+scaffold_policy_name="hello_habichef"
+pkg_name=chef-base
+pkg_origin=cuddletech
+pkg_version="0.0.1"
+pkg_maintainer="@benr"
+pkg_license=("Apache-2.0")
+pkg_description="HabiChef Hello World"
+pkg_upstream_url="http://chef.io"
+pkg_scaffolding="core/scaffolding-chef"
